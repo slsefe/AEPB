e142ad6bef91e066a58e23ab2ebd3b9eca390fef
baiyin <17693374267@163.com>
2021-10-18 22:40:45 +0800

    [baiyin] feat: add parking boy, add parking lot number for parking ticket

---
+++ src/main/java/com/example/AEPB/parkingLot/ParkingBoy.java
@@ -0,0 +1,17 @@
+package com.example.AEPB.parkingLot;
+
+public class ParkingBoy {
+    private final ParkingLot[] parkingLots = new ParkingLot[10];
+
+    public ParkingLot[] getParkingLots() {
+        return parkingLots;
+    }
+
+    public ParkingTicket park(Vehicle vehicle) {
+        return null;
+    }
+
+    public Vehicle pickUp(ParkingTicket parkingTicket) {
+        return null;
+    }
+}
--- src/main/java/com/example/AEPB/parkingLot/ParkingTicket.java
+++ src/main/java/com/example/AEPB/parkingLot/ParkingTicket.java
@@ -4,10 +4,30 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class ParkingTicket {
+    private static final int MIN_NUMBER_OF_PARKING_LOT = 1;
+    private static final int MAX_NUMBER_OF_PARKING_LOT = 10;
+
     private final String ticketNumber;
+    private int parkingLotNumber = 1;
 
     public ParkingTicket() {
         this.ticketNumber = UUID.randomUUID().toString();
+    }
+
+    public ParkingTicket(int parkingLotNumber) {
+        if (parkingLotNumber < MIN_NUMBER_OF_PARKING_LOT || parkingLotNumber > MAX_NUMBER_OF_PARKING_LOT) {
+            throw new IllegalArgumentException("parking lot number should between " + MIN_NUMBER_OF_PARKING_LOT + " and " + MAX_NUMBER_OF_PARKING_LOT);
+        }
+        this.ticketNumber = UUID.randomUUID().toString();
+        this.parkingLotNumber = parkingLotNumber;
+    }
+
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public int getParkingLotNumber() {
+        return parkingLotNumber;
     }
 
     @Override
@@ -19,11 +39,11 @@ public class ParkingTicket {
             return false;
         }
         ParkingTicket that = (ParkingTicket) o;
-        return ticketNumber.equals(that.ticketNumber);
+        return parkingLotNumber == that.parkingLotNumber && ticketNumber.equals(that.ticketNumber);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ticketNumber);
+        return Objects.hash(ticketNumber, parkingLotNumber);
     }
 }
