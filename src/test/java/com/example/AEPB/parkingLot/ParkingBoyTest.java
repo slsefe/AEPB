0f4015cf7bc4c591d0cb38c978042f1fa96af2fa
baiyin <17693374267@163.com>
2021-10-18 23:38:58 +0800

    [baiyin] test: should_park_vehicle_in_parking_lot_1_when_park_given_parking_lot_1_is_not_full_and_parking_lot_2_is_full

--- src/test/java/com/example/AEPB/parkingLot/ParkingBoyTest.java
+++ src/test/java/com/example/AEPB/parkingLot/ParkingBoyTest.java
@@ -33,4 +33,22 @@ class ParkingBoyTest {
         assertEquals(2, parkingTicket.getParkingLotNumber());
     }
 
+    @Test
+    void should_park_vehicle_in_parking_lot_1_when_park_given_parking_lot_1_is_not_full_and_parking_lot_2_is_full() {
+        // given
+        final ParkingBoy parkingBoy = new ParkingBoy();
+        final Vehicle firstVehicle = new Vehicle();
+        final ParkingTicket firstParkingTicket = parkingBoy.park(firstVehicle);
+        for (int i = 1; i < 51; i++) {
+            parkingBoy.park(new Vehicle());
+        }
+        parkingBoy.pickUp(firstParkingTicket);
+
+        // when
+        final ParkingTicket parkingTicket = parkingBoy.park(new Vehicle());
+
+        // then
+        assertEquals(1, parkingTicket.getParkingLotNumber());
+    }
+
 }
