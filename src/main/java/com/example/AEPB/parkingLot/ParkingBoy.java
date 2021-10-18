fcd5693eaf1c1c05cd886635548d9f5c74afe829
baiyin <17693374267@163.com>
2021-10-18 23:22:12 +0800

    [baiyin] feat: should_park_vehicle_in_parking_lot_1_when_park_given_all_parking_lot_is_empty

--- src/main/java/com/example/AEPB/parkingLot/ParkingBoy.java
+++ src/main/java/com/example/AEPB/parkingLot/ParkingBoy.java
@@ -1,7 +1,14 @@
 package com.example.AEPB.parkingLot;
 
 public class ParkingBoy {
-    private final ParkingLot[] parkingLots = new ParkingLot[10];
+    private final static int NUMBER_OF_PARKING_LOT = 10;
+    private final ParkingLot[] parkingLots = new ParkingLot[NUMBER_OF_PARKING_LOT];
+
+    public ParkingBoy() {
+        for (int i = 0; i < NUMBER_OF_PARKING_LOT; i++) {
+            parkingLots[i] = new ParkingLot();
+        }
+    }
 
     public ParkingLot[] getParkingLots() {
         return parkingLots;
